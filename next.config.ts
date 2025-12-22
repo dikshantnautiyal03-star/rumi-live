a933c01c8668b483de8b92afb25364765dbfba21
Dikshant Nautiyal <dixant07@gmail.com>
2025-12-22 20:55:29 +0530

    fix: nextconfig updated for CORS errors

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,16 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        // Apply to all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://rumi-live.web.app' }, // Replace with your actual origin
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+      {
         // Apply to all static game files
         source: '/games/:path*',
         headers: [
